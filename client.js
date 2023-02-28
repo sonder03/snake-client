18a91ffe5e174b1f33a62fc88a13e367e6fcf87d
sonder03 <ravneet.virk@gmail.com>
2023-02-27 21:00:12 -0500

    Save client and play

---
+++ client.js
@@ -0,0 +1,16 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host:  'localhost',
+    port:  50541
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+module.exports = {connect}
--- play.js
+++ play.js
@@ -1,20 +1,7 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host:  'localhost',
-    port:  50541
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
+const clientConnection = require("./client.js");
 
 console.log("Connecting ...");
-var conn = connect();
+var conn = clientConnection.connect();
 
 
 conn.on("connect", () => {
