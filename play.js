30e78fdb5a9c133100fb7d7f2b0fdd6964f41af6
sonder03 <ravneet.virk@gmail.com>
2023-02-27 21:09:24 -0500

    Save name

--- play.js
+++ play.js
@@ -7,10 +7,11 @@ var conn = clientConnection.connect();
 conn.on("connect", () => {
   // code that does something when the connection is first established
 
-  console.log("connection Established");
+  console.log("Successfully connected to game server");
 
+  conn.write("Name: RKV");
+  
 });
-
 
 conn.on("data", (message) => {
   // code that does something when the connection is first established
